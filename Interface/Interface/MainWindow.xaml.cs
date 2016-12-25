d7cd3d643500920b80a6602aa351a6a32c2fbda6
Alexandr Nikolaev <inikolaev1234567890@gmail.com>
2016-12-25 21:27:10 +0700

    Подключил интерфейс (wpf)
    Создал основной фрейм и два доп окна:
    1)Для ввода check-кода
    2)Для ввода капчи

---
+++ Interface/Interface/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using vkapi;
+
+
+namespace Interface
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Settings set = new Settings();
+            set.ClientId = "5783292";
+            set.ClientSecret = "C3id6lTN0wQVmnU5Urtw";
+            set.Permissions = new string[] { "music", "message" };
+            WPF_Event w1 = new WPF_Event();
+            Auth a1 = new Auth(set, w1);
+            a1.LoadAccessToken();
+        }
+
+        private void send_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Interface/Interface/VKapi/JsonScheme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vkapi
+{
+
+    [Serializable]
+    public class AccessToken
+    {
+        public string access_token = null;
+        public long expires_in = 0;
+        public string user_id = null;
+    }
+}
